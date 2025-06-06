d36dfe0c873018342c9b52ca63f29fddb83b3a6d
Soonyoung Park <soon.syp@gmail.com>
2025-06-06 22:27:16 +0200

    test

--- app/db.ts
+++ app/db.ts
@@ -7,21 +7,16 @@ interface MainRow {
   current_amount: number
 }
 
+const sql = neon(`${process.env.DATABASE_URL}`)
+
 export async function checkDbConnection() {
-  if (!process.env.DATABASE_URL) {
-    return "No DATABASE_URL environment variable";
-  }
-  try {
-    const sql = neon(process.env.DATABASE_URL);
-    const result = await sql`SELECT version()`;
-    console.log("Pg version:", result);
+
+    const result = await sql`SELECT version()`
+    console.log("Pg version:", result)
 
     const rawMainData = await sql`SELECT * FROM main WHERE id = 1`
     const data = rawMainData as MainRow[]
     console.log(rawMainData)
     return data[0].current_amount
-  } catch (error) {
-    console.error("Error connecting to the database:", error);
-    return "Database not connected";
-  }
+
 }

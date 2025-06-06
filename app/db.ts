a9dca513438e695dd1f2bc5feaae98938bf7a79e
Soonyoung Park <soon.syp@gmail.com>
2025-06-06 21:58:11 +0200

    test

--- app/db.ts
+++ app/db.ts
@@ -1,5 +1,12 @@
 import { neon } from "@neondatabase/serverless";
 
+interface MainRow {
+  id: number
+  goal: number
+  cup_index: number
+  current_amount: number
+}
+
 export async function checkDbConnection() {
   if (!process.env.DATABASE_URL) {
     return "No DATABASE_URL environment variable";
@@ -8,7 +15,11 @@ export async function checkDbConnection() {
     const sql = neon(process.env.DATABASE_URL);
     const result = await sql`SELECT version()`;
     console.log("Pg version:", result);
-    return "Database connected";
+
+    const rawMainData = await sql`SELECT * FROM main WHERE id = 1`
+    const data = rawMainData as MainRow[]
+    console.log(rawMainData)
+    return data[0].current_amount
   } catch (error) {
     console.error("Error connecting to the database:", error);
     return "Database not connected";

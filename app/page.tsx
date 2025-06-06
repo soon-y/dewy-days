4747c45e6a8843c6f39631e00649c8686c8cb7f1
Soonyoung Park <soon.syp@gmail.com>
2025-06-06 22:01:10 +0200

    test

--- app/page.tsx
+++ app/page.tsx
@@ -112,10 +112,7 @@ export default async function Home() {
             ))}
           </ul>
           <span
-            className={`rounded-full border px-3 py-1.5 text-xs font-semibold ${
-              result === "Database connected"
-                ? "border-[#00E599]/20 bg-[#00E599]/10 text-[#1a8c66] dark:bg-[#00E599]/10 dark:text-[#00E599]"
-                : "border-red-500/20 bg-red-500/10 text-red-500 dark:text-red-500"
+            className={`rounded-full border px-3 py-1.5 text-xs font-semibold ${"border-red-500/20 bg-red-500/10 text-red-500 dark:text-red-500"
             }`}
           >
             {result}

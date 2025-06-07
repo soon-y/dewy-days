5de10279ba433ae15ecf9f412624984c18a29abd
Soonyoung Park <soon.syp@gmail.com>
2025-06-07 12:46:13 +0200

    add error handling

--- app/timeline/page.tsx
+++ app/timeline/page.tsx
@@ -1,17 +1,33 @@
 import Timeline from "@/pages/timetline-client"
 import { neon } from '@neondatabase/serverless'
 import { TimelineRow, MainRow } from "@/types"
+import Alert from "@/components/alert"
 
 export default async function Home() {
-  const sql = neon(`${process.env.DATABASE_URL}`)
-  const timelineData = await sql`SELECT * FROM timeline`
-  const timeline = timelineData as TimelineRow[]
-  const currentAmountData = await sql`SELECT * FROM main WHERE id = 1`
-  const currentAmount = currentAmountData as MainRow[]
+  try {
+    const sql = neon(`${process.env.DATABASE_URL}`)
 
-  return (
-    <>
+    const [timelineData, currentAmountData] = await Promise.all([
+      sql`SELECT * FROM timeline`,
+      sql`SELECT * FROM main WHERE id = 1`
+    ])
+
+    const timeline = timelineData as TimelineRow[]
+    const currentAmount = currentAmountData as MainRow[]
+
+    if (!timeline.length || !currentAmount.length) {
+      throw new Error("Timeline or main data not found")
+    }
+
+    return (
       <Timeline data={timeline} main={currentAmount[0]} />
-    </>
-  )
+    )
+  } catch (error) {
+    console.error("Failed to fetch data:", error)
+    return (
+      <div className={`w-full h-full fixed overflow-hidden p-6 gradient`}>
+        <Alert />
+      </div>
+    )
+  }
 }

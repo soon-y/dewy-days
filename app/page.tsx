4d7fbee788b75e43e7169001787b3510e1ffcc39
Soonyoung Park <soon.syp@gmail.com>
2025-06-21 15:21:27 +0200

    fetch only last date from timeline

--- app/page.tsx
+++ app/page.tsx
@@ -11,27 +11,24 @@ export default async function Home() {
 
     const [rawMainData, timelineData] = await Promise.all([
       sql`SELECT * FROM main WHERE id = 1`,
-      sql`SELECT date FROM timeline`
+      sql`SELECT date FROM timeline ORDER BY id DESC LIMIT 1`
     ])
 
     if (!rawMainData.length || !timelineData.length) {
       throw new Error("Missing data")
     }
 
-    const main = rawMainData as MainRow[]
-
     const cupData = await sql`SELECT amount FROM cup WHERE id = ${rawMainData[0].cup_index + 1}`
     if (!cupData.length) throw new Error("Missing cup data")
-    const cup = cupData as { amount: number }[]
-
-    const timeline = timelineData as { date: Date }[]
-    const lastDate = new Date(timeline[timeline.length - 1].date).toLocaleDateString()
+    const cup = cupData[0] as { amount: number }
+    const main = rawMainData[0] as MainRow
+    const timeline = timelineData[0] as { date: Date }
+    const lastDate = new Date(timeline.date).toLocaleDateString()
     const today = new Date().toLocaleDateString()
     const reset: boolean = today.includes(lastDate) ? false : true
-    const waterHeight = reset? 100 : main[0].current_amount >= main[0].goal ? 0 : 100 - (main[0].current_amount / main[0].goal * 100)
+    const waterHeight = reset? 100 : main.current_amount >= main.goal ? 0 : 100 - (main.current_amount / main.goal * 100)
 
-
-    return <App main={main[0]} cup={cup[0]} reset={reset} waterHeightInit={waterHeight} />
+    return <App main={main} cup={cup} reset={reset} waterHeightInit={waterHeight} />
   } catch (error) {
     console.error("Error loading data:", error)
     return (
